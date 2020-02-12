b527636a919d6916899c828a16628534d50b194b
zwyak <vlad.senyuk97@gmail.com>
2020-02-12 11:26:54 +0300

    Updated project files

---
+++ articles.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const utils = require('./utils.js');
+let articles = require('./articles.json');
+
+function arReadAll(req, res, payload, cb) {
+  cb(null, articles);
+}
+
+module.exports.arReadAll = arReadAll
+
+function arRead(req, res, payload, cb) {
+  let found;
+
+  for (var i = 0; i < articles.length; i++) {
+    if (articles[i].id == payload.id){
+      found = articles[i];
+      break;
+    }
+  }
+
+  cb(null, found);
+}
+
+module.exports.arRead = arRead
+
+function arCreate(req, res, payload, cb) {
+  const id = Date.now();
+  const result = {id:id, title:payload.title, text:payload.text, date: Date.now(), author: payload.author, comments: []};
+
+  if (!payload.title || !payload.text || !payload.author){
+    cb(null, { code: 400, message: "Request invalid"});
+    return;
+  }
+
+  articles.push(result);
+  utils.writeJson('./articles.json', JSON.stringify(articles));
+  articles = require('./articles.json');
+
+  cb(null, result);
+}
+
+module.exports.arCreate = arCreate
+
+function arUpdate(req, res, payload, cb) {
+  let found;
+
+  for (var i = 0; i < articles.length; i++) {
+    if (articles[i].id == payload.id){
+      found = articles[i];
+
+      if (payload.title) found.title = payload.title;
+      if (payload.text) found.text = payload.text;
+      if (payload.author) found.author = payload.author;
+
+      articles[i] = found;
+      break;
+    }
+  }
+
+  if (found){
+    utils.writeJson('./articles.json', JSON.stringify(articles));
+    articles = require('./articles.json');
+    cb(null, found);
+  }else{
+    cb(null, { code: 400, message: "Request invalid"});
+  }
+}
+
+module.exports.arUpdate = arUpdate
+
+function arDelete(req, res, payload, cb) {
+  let found;
+
+  for (var i = 0; i < articles.length; i++) {
+    if (articles[i].id == payload.id){
+      found = articles[i];
+      break;
+    }
+  }
+
+  if (found){
+    const index = articles.indexOf(found);
+    articles.splice(index, 1);
+
+    utils.writeJson('./articles.json', JSON.stringify(articles));
+    articles = require('./articles.json');
+    cb(null, found);
+  }else{
+    cb(null, { code: 400, message: "Request invalid"});
+  }
+
+}
+
+module.exports.arDelete = arDelete
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const http = require('http');
-const fs = require('fs');
-let articles = require('./articles');
+let articles = require('./articles.json');
+const ar = require('./articles.js');
+const com = require('./comments.js');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const handlers = {
-  '/api/articles/readall' : arReadAll,
-  '/api/articles/read' : arRead,
-  '/api/articles/create': arCreate,
-  '/api/articles/update': arUpdate,
-  '/api/articles/delete': arDelete,
-  '/api/comments/create': comCreate,
-  '/api/comments/delete': comDelete
+  '/api/articles/readall' : ar.arReadAll,
+  '/api/articles/read' : ar.arRead,
+  '/api/articles/create': ar.arCreate,
+  '/api/articles/update': ar.arUpdate,
+  '/api/articles/delete': ar.arDelete,
+  '/api/comments/create': com.comCreate,
+  '/api/comments/delete': com.comDelete
 };
 
 const server = http.createServer((req, res) => {
@@ -59,165 +60,4 @@ function parseBodyJson(req, cb) {
 
     cb(null, params);
   });
-}
-
-function arReadAll(req, res, payload, cb) {
-  cb(null, articles);
-}
-
-function arRead(req, res, payload, cb) {
-  let found;
-
-  for (var i = 0; i < articles.length; i++) {
-    if (articles[i].id == payload.id){
-      found = articles[i];
-      break;
-    }
-  }
-
-  cb(null, found);
-}
-
-function arCreate(req, res, payload, cb) {
-  const id = Date.now();
-  const result = {id:id, title:payload.title, text:payload.text, date: Date.now(), author: payload.author, comments: []};
-
-  if (!payload.title || !payload.text || !payload.author){
-    cb(null, { code: 400, message: "Request invalid"});
-    return;
-  }
-
-  articles.push(result);
-  writeJson('./articles.json', JSON.stringify(articles));
-  articles = require('./articles.json');
-
-  cb(null, result);
-}
-
-function arUpdate(req, res, payload, cb) {
-  let found;
-
-  for (var i = 0; i < articles.length; i++) {
-    if (articles[i].id == payload.id){
-      found = articles[i];
-
-      if (payload.title) found.title = payload.title;
-      if (payload.text) found.text = payload.text;
-      if (payload.author) found.author = payload.author;
-
-      articles[i] = found;
-      break;
-    }
-  }
-
-  if (found){
-    writeJson('./articles.json', JSON.stringify(articles));
-    articles = require('./articles.json');
-    cb(null, found);
-  }else{
-    cb(null, { code: 400, message: "Request invalid"});
-  }
-}
-
-function arDelete(req, res, payload, cb) {
-  let found;
-
-  for (var i = 0; i < articles.length; i++) {
-    if (articles[i].id == payload.id){
-      found = articles[i];
-      break;
-    }
-  }
-
-  if (found){
-    const index = articles.indexOf(found);
-    articles.splice(index, 1);
-
-    writeJson('./articles.json', JSON.stringify(articles));
-    articles = require('./articles.json');
-    cb(null, found);
-  }else{
-    cb(null, { code: 400, message: "Request invalid"});
-  }
-
-}
-
-function comCreate(req, res, payload, cb) {
-  const id = Date.now();
-  const result = {id: id, articleId: payload.articleId, text:payload.text, date: Date.now(), author: payload.author};
-  let found;
-
-  if (!payload.articleId || !payload.text || !payload.author){
-    cb(null, { code: 400, message: "Request invalid"});
-    return;
-  }
-
-  for (var i = 0; i < articles.length; i++) {
-    if (articles[i].id == payload.articleId){
-      found = articles[i];
-      found.comments.push(result);
-      articles[i] = found;
-      break;
-    }
-  }
-
-  if (found){
-    writeJson('./articles.json', JSON.stringify(articles));
-    articles = require('./articles.json');
-    cb(null, result);
-  }else{
-    cb(null, { code: 400, message: "Request invalid"});
-  }
-
-}
-
-function comDelete(req, res, payload, cb) {
-  let foundArticle;
-
-  for (var i = 0; i < articles.length; i++) {
-    if (articles[i].id == payload.articleId){
-      foundArticle = articles[i];
-      break;
-    }
-  }
-
-  let foundCommentIndex;
-  if (foundArticle){
-    for (var i = 0; i < foundArticle.comments.length; i++) {
-      if (foundArticle.comments[i].id == payload.id){
-        foundCommentIndex = foundArticle.comments.indexOf(foundArticle.comments[i]);
-        foundArticle.comments.splice(foundCommentIndex, 1);
-        break;
-      }
-    }
-  }
-
-  if (foundCommentIndex){
-    for (var i = 0; i < articles.length; i++) {
-      if (articles[i].id == payload.articleId){
-        articles[i] = foundArticle;
-        break;
-      }
-    }
-  }
-
-  if (foundCommentIndex){
-    writeJson('./articles.json', JSON.stringify(articles));
-    articles = require('./articles.json');
-    cb(null, foundArticle);
-  }else{
-    cb(null, { code: 400, message: "Request invalid"});
-  }
-
-}
-
-function writeJson(file, data){
-  fs.writeFile(file, data, 'utf8', function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
-
-    console.log("JSON file has been saved.");
-});
 }
